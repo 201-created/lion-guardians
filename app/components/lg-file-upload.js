a7c3379f30a7ba7b6f8ef68dead53d9f8b2044d6
Cory Forsyth <cory.forsyth@gmail.com>
2015-01-21 11:49:22 -0500

    WIP showing percentage values and file list

--- app/components/lg-file-upload.js
+++ app/components/lg-file-upload.js
@@ -3,9 +3,24 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: null,
 
+  uploads: function(){ return Ember.A(); }.property(),
+
+  uploadedBytes: 0,
+  totalBytes: 0,
+
+  percentComplete: function(){
+    var uploadedBytes = this.get('uploadedBytes'),
+        totalBytes    = this.get('totalBytes');
+
+
+    if (totalBytes === 0) { return 0; }
+
+    return parseInt( 100 * (uploadedBytes / totalBytes) );
+  }.property('uploadedBytes', 'totalBytes'),
+
   setupFileUpload: function() {
-    var url = '/';
     var store = this.get('store');
+    var component = this;
 
     var fileInput = this.$('input[type="file"]');
     this.fileupload = fileInput.fileupload({
@@ -16,39 +31,75 @@ export default Ember.Component.extend({
       dataType: 'XML',
       replaceFileInput: false,
 
+      progress: function(e,data){
+        console.log('progress',e,data,data.files[0].name);
+        var upload = data.uploadObj;
+        Ember.run(function(){
+          upload.set('uploadedBytes', data.loaded);
+          upload.set('totalBytes', data.total);
+          upload.set('bitRate', data.bitrate);
+        });
+      },
+
+      progressall: function(e,data){
+        console.log('progressall',e,data);
+        Ember.run(function(){
+          component.setProperties({
+            uploadedBytes: data.loaded,
+            totalBytes: data.total
+          });
+        });
+      },
+
       add: function(e, data){
-        debugger;
         var upload = store.createRecord('upload');
         upload.save().then(function(){
           data.formData = upload.get('fields');
           data.url = upload.get('url');
+
+          data.uploadObj = upload;
+          upload.setProperties({
+            name: data.files[0].name,
+            status: 'queued'
+          });
+
+          Ember.run(function(){
+            component.get('uploads').pushObject(upload);
+          });
 
           data.submit();
         });
       },
 
       submit: function(e, data){
-        console.log('submit',e,data);
-        debugger;
+        console.log('submit',e,data,data.files[0].name);
+        var upload = data.uploadObj;
+        Ember.run(upload, 'set', 'status', 'submitted');
       },
 
       send: function(e, data){
-        console.log('send',e,data);
+        console.log('send',e,data,data.files[0].name);
+        var upload = data.uploadObj;
+        Ember.run(upload, 'set', 'status', 'sent');
+      },
+
+      start: function(e, data){
         debugger;
       },
 
       fail: function(e, data){
-        console.log('fail',e,data);
-        debugger;
+        console.log('fail',e,data,data.files[0].name);
       },
 
       done: function(e, data){
-        console.log('done',e,data);
-        debugger;
+        console.log('done',e,data,data.files[0].name);
+        var upload = data.uploadObj;
+
+        Ember.run(function(){
+          upload.set('status', 'uploaded');
+        });
       }
     });
-
-    window.XXX = this.fileupload;
 
   }.on('didInsertElement')
 });
--- app/models/upload.js
+++ app/models/upload.js
@@ -2,5 +2,19 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   url: DS.attr('string'),
-  fields: DS.attr('raw')
+  fields: DS.attr('raw'),
+
+
+  uploadedBytes: 0,
+  totalBytes: 0,
+
+  percentComplete: function(){
+    var uploadedBytes = this.get('uploadedBytes'),
+        totalBytes    = this.get('totalBytes');
+
+
+    if (totalBytes === 0) { return 0; }
+
+    return parseInt( 100 * (uploadedBytes / totalBytes) );
+  }.property('uploadedBytes', 'totalBytes')
 });
