10d202454dfafcaed100f167c2107e3b5343ac79
Cory Forsyth <cory.forsyth@gmail.com>
2015-01-21 10:32:27 -0500

    WIP working multi-file uploads

--- app/components/lg-file-upload.js
+++ app/components/lg-file-upload.js
@@ -1,39 +1,53 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: null,
+
   setupFileUpload: function() {
     var url = '/';
+    var store = this.get('store');
 
     var fileInput = this.$('input[type="file"]');
     this.fileupload = fileInput.fileupload({
       fileInput: fileInput,
-      url: url,
       type: 'POST',
-      autoUpload: false,
-      formData: {},
+      autoUpload: true,
       paramName: 'file',
       dataType: 'XML',
-      replaceFileInput: false
+      replaceFileInput: false,
+
+      add: function(e, data){
+        debugger;
+        var upload = store.createRecord('upload');
+        upload.save().then(function(){
+          data.formData = upload.get('fields');
+          data.url = upload.get('url');
+
+          data.submit();
+        });
+      },
+
+      submit: function(e, data){
+        console.log('submit',e,data);
+        debugger;
+      },
+
+      send: function(e, data){
+        console.log('send',e,data);
+        debugger;
+      },
+
+      fail: function(e, data){
+        console.log('fail',e,data);
+        debugger;
+      },
+
+      done: function(e, data){
+        console.log('done',e,data);
+        debugger;
+      }
     });
 
-    debugger;
-
-    this.fileupload.on('done', function(e,data){
-      console.log('done',e,data);
-      debugger;
-    });
-
-    this.fileupload.on('fail', function(e, data) {
-      console.log('fail',e,data);
-      debugger;
-    });
-
-    this.fileupload.on('start', function(e,data){
-      console.log('start',e,data);
-      debugger;
-    });
-
-    debugger;
     window.XXX = this.fileupload;
 
   }.on('didInsertElement')
