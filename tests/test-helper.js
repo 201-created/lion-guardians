f1d88b76b9f1d0c6aa4aed055691a82d31eb702e
Cory Forsyth <cory.forsyth@gmail.com>
2015-02-07 18:43:08 -0500

    Upgrade ember-cli to 0.1.12

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -7,12 +7,6 @@ import { clearSession } from '../torii-adapters/lion-guardians';
 
 setResolver(resolver);
 
-document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
-
-QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container' });
-var containerVisibility = QUnit.urlParams.nocontainer ? 'hidden' : 'visible';
-document.getElementById('ember-testing-container').style.visibility = containerVisibility;
-
 QUnit.testStart(function(){
   clearSession();
   FakeServer.start();
