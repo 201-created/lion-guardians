2b1e3978cb11023ab1c32039d6af8ac93ad1a6f8
Isaac Ezer <isaac.ezer@gmail.com>
2015-05-18 18:34:53 -0400

    fix tests, remove .js from import command because that causes mysterious test failures.

--- tests/acceptance/image-set/lion-search-test.js
+++ tests/acceptance/image-set/lion-search-test.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import startApp from '../../helpers/start-app';
-import {stubRequest} from '../../helpers/fake-server.js';
+import {stubRequest} from '../../helpers/fake-server';
 import { stubGetImageSet,
          stubGetOrganizations,
          stubImageSetJSON,
