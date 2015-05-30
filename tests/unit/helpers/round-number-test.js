acd6946a7024c26966f8f45f9b240cddcab655e3
Cory Forsyth <cory.forsyth@gmail.com>
2015-05-30 18:50:52 -0400

    fix failing test

--- tests/unit/helpers/round-number-test.js
+++ tests/unit/helpers/round-number-test.js
@@ -6,6 +6,6 @@ module('RoundNumberHelper');
 
 // Replace this with your real tests.
 test('it works', function() {
-  var result = roundNumber(42);
+  var result = roundNumber(42, {});
   ok(result);
 });
