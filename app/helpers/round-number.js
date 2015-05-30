1530673f02b272be3b1247c2a8d6e152bff1b462
Cory Forsyth <cory.forsyth@gmail.com>
2015-05-30 18:10:07 -0400

    Add `precision` option to round-number

--- app/helpers/round-number.js
+++ app/helpers/round-number.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
-export function roundNumber(input) {
-  return parseInt(input, 10);
+export function roundNumber(input, options) {
+  var precision = Ember.get(options, 'hash.precision') || '0';
+  precision = parseInt(precision, 10);
+
+  return Number(input).toFixed(precision);
 }
 
 export default Ember.Handlebars.makeBoundHelper(roundNumber);
