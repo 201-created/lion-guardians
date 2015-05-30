65eaf476f0ce7d6adfd15d25d0dcb6c9726f13be
Cory Forsyth <cory.forsyth@gmail.com>
2015-05-30 17:18:06 -0400

    add thumbnailUrl, mainUrl to images

--- app/models/image.js
+++ app/models/image.js
@@ -2,6 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   url: DS.attr('string'),
+  thumbnailUrl: DS.attr(),
+  mainUrl: DS.attr(),
   isPublic: DS.attr('boolean'),
   imageType: DS.attr('string'),
   imageSet: DS.belongsTo('image-set', {inverse: 'images'}),
