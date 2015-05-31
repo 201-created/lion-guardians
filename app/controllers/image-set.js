979f50084263d41461706b066967cbd1bc519825
Cory Forsyth <cory.forsyth@gmail.com>
2015-05-31 18:07:02 -0400

    Added Create Lion link in header nav, ability to create lion on image sets page

--- app/controllers/image-set.js
+++ app/controllers/image-set.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 import {imageTypes} from 'lion-guardians/utils/units';
 
 export default Ember.Controller.extend({
+  queryParams: ['createLion'],
+  createLion: false,
+
   currentUser: null,
   imageTypes: imageTypes,
   uploadImageType: imageTypes[0],
@@ -22,6 +25,10 @@ export default Ember.Controller.extend({
     return !this.get('model.id') && this.get('model.images.length');
   }.property('model.id', 'model.images.length'),
 
+  isCreatingNewLion: function(){
+    return !!this.get('createLion');
+  }.property('createLion'),
+
   actions: {
     addImage: function(upload){
       var url = upload.get('url'),
@@ -36,17 +43,55 @@ export default Ember.Controller.extend({
     },
 
     saveImageSet: function() {
-      var imageSet = this.get('model'),
-          controller = this;
-      imageSet.save().then(function(imageSet) {
-        // Remove original images that don't have IDs.
-        // The server will come back with new objects
-        var images = imageSet.get('images');
-        var nullImages = images.rejectBy('id');
-        images.removeObjects(nullImages);
+      const controller = this;
 
-        controller.transitionToRoute('image-set', imageSet);
-      });
+      if (this.get('isCreatingNewLion')) {
+        this._createNewLionWithImageSet().then(function(lion){
+          return controller.transitionToRoute('lion', lion);
+        });
+      } else {
+        this._saveOrCreateImageSet().then(function(imageSet){
+          controller.transitionToRoute('image-set', imageSet);
+        });
+      }
     }
+  },
+
+  _createNewLionWithImageSet: function(){
+    // if we are creating a lion:
+    //   * create lion record (unsaved)
+    //   * create the image set
+    //   * set lion's primaryImageSet to the image set
+    //   * save the lion
+
+    const lionName = this.get('lionName');
+    const lion = this.get('store').createRecord('lion', {name: lionName});
+    let imageSet;
+
+    return this._saveOrCreateImageSet().then(function(_imageSet) {
+      imageSet = _imageSet;
+
+      lion.set('primaryImageSet', imageSet);
+      return lion.save();
+    });
+  },
+
+  _saveOrCreateImageSet: function(){
+    var imageSet = this.get('model'),
+        controller = this;
+
+    return imageSet.save().then(function(imageSet) {
+      imageSet = controller._removeEmptyImages(imageSet);
+      return imageSet;
+    });
+  },
+
+  // Remove original images that don't have IDs.
+  // The server will come back with new objects
+  _removeEmptyImages: function(imageSet){
+    var images = imageSet.get('images');
+    var nullImages = images.rejectBy('id');
+    images.removeObjects(nullImages);
+    return imageSet;
   }
 });
