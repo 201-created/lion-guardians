a42eb2b6b4019c782ea714776315abb7c447987b
Lauren <lpellsworth@gmail.com>
2015-05-31 16:13:39 -0400

    Change Primary Image Set header to include lion name consistently

--- app/components/lg-image-set-summary.js
+++ app/components/lg-image-set-summary.js
@@ -4,8 +4,15 @@ export default Ember.Component.extend({
   imageSet: null,
   classNames: ['lg-image-set-summary'],
   selectedImage: Ember.computed.reads('imageSet.mainImage'),
-  setName: null,
   heroImage: false,
+
+  setName: function() {
+    if (this.get('imageSet.isPrimary')) {
+      return 'Primary Image Set for ' + this.get('imageSet.lion.name');
+    } else {
+      return 'Image Set ID ' + this.get('imageSet.id');
+    }
+  }.property('imageSet.isPrimary', 'imageSet.lion.name', 'imageSet.id'),
 
   verificationStatus: function() {
     return this.get('imageSet.isVerified') ? 'Verified' : 'Unverified';
