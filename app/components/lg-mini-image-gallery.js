2e6173a55089ad158acd5dc4814b116cf58ebc91
Cory Forsyth <cory.forsyth@gmail.com>
2015-06-07 12:41:29 -0400

    Fix styling of lg-mini-image, limit to 4 thumbnail images shown

--- app/components/lg-mini-image-gallery.js
+++ app/components/lg-mini-image-gallery.js
@@ -3,20 +3,27 @@ import {imageTypesForFilter} from 'lion-guardians/utils/units';
 
 export default Ember.Component.extend({
   imageSet: null,
+  maxImageCount: 4,
+
   selectedImage: null,
   imageTypes: imageTypesForFilter,
   selectedImageType: imageTypesForFilter[0],
   hasFilteredImages: Ember.computed.gt('filteredImages.length', 0),
 
   filteredImages: function() {
-    var selectedImageType = this.get('selectedImageType'),
-        images = this.get('imageSet.images');
+    const selectedImageType = this.get('selectedImageType'),
+          images = this.get('imageSet.images'),
+          maxImageCount = this.get('maxImageCount');
+
+    let filteredImages;
 
     if (selectedImageType === 'all') {
-      return images;
+      filteredImages = images;
     } else {
-      return images.filterBy('imageType', selectedImageType);
+      filteredImages = images.filterBy('imageType', selectedImageType);
     }
+
+    return filteredImages.slice(0,maxImageCount);
   }.property('selectedImageType', 'imageSet.images.[]'),
 
   actions: {
